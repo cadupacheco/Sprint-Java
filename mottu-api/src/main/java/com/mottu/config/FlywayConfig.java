7f3a5eca226e1943cce54dc1a71c4993b157a636
Pedro <pedroacladeira@gmail.com>
2025-09-29 00:45:38 -0300

    ajustando erros

--- mottu-api/src/main/java/com/mottu/config/FlywayConfig.java
+++ mottu-api/src/main/java/com/mottu/config/FlywayConfig.java
@@ -1,26 +1,27 @@
-package com.mottu.config;
-
-import org.flywaydb.core.Flyway;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-@Configuration
-public class FlywayConfig {
-
-    @Bean
-    public Flyway flyway() {
-        Flyway flyway = Flyway.configure()
-                .schemas("YOUR_SCHEMA")                         // se múltiplos, use .schemas("schema1","schema2")
-                .locations("classpath:db/migration")             // aceita varargs ou List.of(...)
-                .baselineOnMigrate(true)
-                .baselineVersion("0")
-                .validateOnMigrate(false)
-                .encoding(StandardCharsets.UTF_8)                // usa Charset, não String
-                .load();
-        flyway.migrate();
-        return flyway;
-    }
-}
+//package com.mottu.config;
+//
+//import org.flywaydb.core.Flyway;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//import java.nio.charset.StandardCharsets;
+//
+//@Configuration
+//public class FlywayConfig {
+//
+//    @Bean
+//    public Flyway flyway(DataSource dataSource) {
+//        Flyway flyway = Flyway.configure()
+//                .dataSource(dataSource)  // <- ESSENCIAL
+//                .locations("classpath:db/migration")
+//                .baselineOnMigrate(true)
+//                .baselineVersion("0")
+//                .validateOnMigrate(false)
+//                .encoding(StandardCharsets.UTF_8)
+//                .load();
+//        flyway.migrate();
+//        return flyway;
+//    }
+//
+//}
